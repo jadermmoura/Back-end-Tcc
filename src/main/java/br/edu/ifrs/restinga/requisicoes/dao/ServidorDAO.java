d7a70f02c18f025aa9bf8b30a5b76df48dae9e01
jimalvess <jimalvess@gmail.com>
2019-09-12 20:15:00 -0300

    Criando classes DAO, controller e ajeitando nome do projeto

---
+++ src/main/java/br/edu/ifrs/restinga/requisicoes/dao/ServidorDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifrs.restinga.requisicoes.dao;
+
+import br.edu.ifrs.restinga.requisicoes.modelo.Servidor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServidorDAO extends CrudRepository<Servidor, Integer> {
+    
+}
