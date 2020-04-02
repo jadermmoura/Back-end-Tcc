4bb93efac5866f833a14f4c54d5621ea57a6d1c3
unknown <jader.mmoura@gmail.com>
2020-04-01 23:52:42 -0300

    alterei para toUpperCase para melhor visualização dos nomes dos aluno

--- src/main/java/br/edu/ifrs/restinga/requisicoes/controle/UsuariosControle.java
+++ src/main/java/br/edu/ifrs/restinga/requisicoes/controle/UsuariosControle.java
@@ -26,7 +26,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -136,6 +135,8 @@ public class UsuariosControle {
     public Usuario novoUsuario(@RequestBody Usuario usuario) {
         usuario.setSenha(PASSWORD_ENCODER.encode(usuario.getNovaSenha()));
         usuario.setPermissoes("aluno");
+        String novoNome = usuario.getNome().toUpperCase();
+        usuario.setNome(novoNome);
         Usuario usuarioBanco = usuarioDAO.findByLogin(usuario.getLogin());
         if (usuarioBanco != null) {
             throw new RequisicaoInvalida("este login ja existe");
